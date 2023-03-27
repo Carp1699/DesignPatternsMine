9d07a04b90ecdc31b2627c47fdd1de8fe9167bd7
pelayo <crodriguez@griddynamics.com>
2023-03-27 11:50:02 -0600

    Second commit

--- src/main/java/behavorial/Memento/Editor.java
+++ src/main/java/behavorial/Memento/Editor.java
@@ -1,4 +1,12 @@
 package behavorial.Memento;
 
+import java.util.ArrayList;
+
 public class Editor{
+
+
+    public static void main(String[] args) {
+        ArrayList<Integer> uwu = new ArrayList<>();
+        uwu.add(32);
+    }
 }
